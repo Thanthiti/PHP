8cd034f667ea308e01a6d001c761fdcb6c655f3c
Thanthiti <plam1234palm@gmail.com>
2023-12-16 17:23:00 +0700

    Te

--- 1W/Test.php
+++ 1W/Test.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-    <?php
+  <?php
+  
+  $id = "6506021622159";
+  $Name = "ธัญธิติ ปิ่นโมลี";
+  $Adress = "14 อนามัยงามเจริญ4 แขวงท่าข้าม เขตบางขุนเทียน กรุงเทพ 10150";
+  $Phone= "0945255469";
+  $คณะ= "อุตสาหกรรมและเทคโนโลยี";
+  $สาขา="เทคโนโลยีสารสนเทศ";
+  $หลักสูตร="วิทยาศาตร์บัณฑิต";
+  echo ("$id <br>"); 
+  echo ("$Name <br>");
+  echo ($Adress);
+  echo ($Phone);
+  echo ($คณะ);
+  echo ($สาขา);
+  echo ($หลักสูตร);
+  
 
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
-    echo "Thanthiti"
     ?>
 </body>
 </html>
