2500b44f213f0292bc83fe9ad7cceea284b30b4a
Thanthiti <plam1234palm@gmail.com>
2023-12-22 12:37:42 +0700

    Lecture07

---
+++ 5W/function.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p{
+            font-size: 60px;
+        }
+    </style>
+    
+</head>
+
+<body>
+    
+    <p>
+        
+    <?php
+    function add($a,$b ){
+        $result = $a - $b;
+        echo " <br><br> Result add : $result";
+    }
+    add(10,3);
+    ?></p>
+    
+    
+
+
+
+</body>
+</html>
